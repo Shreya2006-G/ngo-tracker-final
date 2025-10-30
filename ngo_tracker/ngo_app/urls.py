4afce0eda37d2fff264cbd755d8379b33a6e584e
Your Name <your-email@example.com>
2025-10-31 00:13:04 +0530

    Fixed admin URL duplicate warning and configured project settings

--- ngo_tracker/ngo_app/urls.py
+++ ngo_tracker/ngo_app/urls.py
@@ -1,7 +1,4 @@
-from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
-    path('admin/', admin.site.urls),                        # Django admin
     path('ngo_dashboard/', include('ngo_dashboard.urls')),  # NGO dashboard app
 ]
--- ngo_tracker/ngo_tracker/settings.py
+++ ngo_tracker/ngo_tracker/settings.py
@@ -1,3 +1,17 @@
+"""
+Django settings for ngo_tracker project.
+"""
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-my%164dr2e(9*pe!1&i0iv^zsa@_t+qlde_wbscuy02@^h3fm4'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -9,3 +23,57 @@ INSTALLED_APPS = [
     'ngo_dashboard',
     'users',
 ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'ngo_tracker.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'ngo_tracker.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
